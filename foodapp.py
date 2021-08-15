dc188a0eac08fda8f2111c69133ed5d486217b22
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2021-08-15 15:27:11 +0530

    Building food app 2D

---
+++ foodapp.py
@@ -0,0 +1,13 @@
+print("Welcome U all to my food app")
+ans=input("\nAre you new to food app? (y/n): ")
+if ans=='y':
+    print("\nCalling")
+print("\nWelcome back again to our food app ")
+name=input("Enter your existing user name : ")
+while True:
+    passw=input("\nEnter your password : ")
+    if passw=='1234':
+        print("Successfully ")
+        break
+    else:
+        print("Try Again ")
